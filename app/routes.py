b9eee06692e91f2b11a2345f32ffc0b7bdf56b58
Megan Gonzalez <meganmgonzalez18@icloud.com>
2023-04-22 22:54:15 -0400

    Error handling 404 and 400

--- app/routes.py
+++ app/routes.py
@@ -24,3 +24,21 @@ def handle_books():
             "description": book.description
         }), 200
     return jsonify(books_response)
+
+@books_bp.route("/<book_id>", methods = ["GET"])
+def handle_book(book_id):
+
+    try:
+        book_id = int(book_id)
+    except:
+        return {"message": f"book {book_id} invalid"}, 400
+
+    for book in books:
+        if book.id == book_id:
+            return {
+                "id": book.id,
+                "title": book.title,
+                "description": book.description
+            }, 200
+
+    return {"message":f"book {book_id} not found"}, 404
