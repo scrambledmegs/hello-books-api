f9629dbc3631b08557940106d542b9d00f8d7fd5
Megan Gonzalez <meganmgonzalez18@icloud.com>
2023-04-22 23:06:26 -0400

    Refactored Error Handling 404 and 400. Added helper function.

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
 
 class Book:
     def __init__(self, id, title, description):
@@ -27,18 +27,21 @@ def handle_books():
 
 @books_bp.route("/<book_id>", methods = ["GET"])
 def handle_book(book_id):
+    book = validate_book(book_id)
+    return {
+            "id": book.id,
+            "title": book.title,
+            "description": book.description
+            }
 
+def validate_book(book_id):
     try:
         book_id = int(book_id)
     except:
-        return {"message": f"book {book_id} invalid"}, 400
+        abort(make_response({"message":f"book {book_id} invalid"}, 400))
 
     for book in books:
         if book.id == book_id:
-            return {
-                "id": book.id,
-                "title": book.title,
-                "description": book.description
-            }, 200
-
-    return {"message":f"book {book_id} not found"}, 404
+            return book
+    
+    abort(make_response({"message":f"book {book_id} not found"}, 404))
