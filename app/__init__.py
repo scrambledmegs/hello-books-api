0991e5f07ec69ca30ec07f82521ef76279a74728
Megan Gonzalez <meganmgonzalez18@icloud.com>
2023-05-09 10:25:30 -0400

    testing, refactoring

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,28 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from dotenv import load_dotenv
+import os
 
 db = SQLAlchemy()
 migrate = Migrate()
-# postgresql+psycopg2://postgres:postgres@localhost:5432/hello_books
+load_dotenv()
 
 def create_app(test_config=None):
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/hello_books'
+    if not test_config:
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    else:
+        app.config['TESTING'] = True
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_TEST_DATABASE_URI")
+
 
     db.init_app(app)
     migrate.init_app(app, db)
+
     from app.models.book import Book
 
     from .routes import books_bp
--- app/routes.py
+++ app/routes.py
@@ -2,18 +2,6 @@ from app import db
 from app.models.book import Book
 from flask import Blueprint, jsonify, make_response, request, abort
 
-# class Book:
-#     def __init__(self, id, title, description):
-#         self.id = id
-#         self.title = title
-#         self.description = description
-
-# books = [
-#     Book(1, 'Something Borrowed', 'Romance Novel'),
-#     Book(2, 'Harry Potter and The Goblet of Fire', 'Fantasy'),
-#     Book(3, 'Act Like a Lady, Think Like a Man', 'Self Help')
-# ]
-
 books_bp = Blueprint("books", __name__, url_prefix = "/books")
 
 def validate_book(book_id):
@@ -36,37 +24,31 @@ def validate_book(book_id):
 @books_bp.route("", methods=["POST"])
 def create_book():
     request_body = request.get_json()
-    new_book = Book(
-        title = request_body["title"],
-        description = request_body["description"]
-    )
+    new_book = Book.from_dict(request_body)
 
     db.session.add(new_book)
     db.session.commit()
     
-    return make_response(f"Book {new_book.title} successfully created.", 201)
+    return make_response(jsonify(f"Book {new_book.title} successfully created"), 201)
 
 
-@books_bp.route("", methods=["GET", "POST"])
+@books_bp.route("", methods=["GET"])
 def read_all_books():
-    books = Book.query.all()
+    title_query = request.args.get("title")
+    if title_query:
+        books = Book.query.filter_by(title=title_query)
+    else:
+        books = Book.query.all()
+
     books_response = []
     for book in books:
-        books_response.append({
-            "id": book.id,
-            "title": book.title,
-            "description": book.description
-        })
+        books_response.append(book.to_dict())
     return jsonify(books_response)
 
 @books_bp.route("/<book_id>", methods=["Get"])
 def read_one_book(book_id):
     book = validate_book(book_id)
-    return {
-        "id": book.id,
-        "title": book.title,
-        "description": book.description
-    }
+    return book.to_dict()
 
 @books_bp.route("/<book_id>", methods=["PUT"])
 def update_book(book_id):
@@ -77,7 +59,7 @@ def update_book(book_id):
     book.description = request_body["description"]
 
     db.session.commit()
-    return make_response(f"Book {book.id} successfully updated.")
+    return make_response(jsonify(f"Book {book.id} successfully updated."))
 
 @books_bp.route("/<book_id>", methods=["DELETE"])
 def delete_book(book_id):
@@ -86,4 +68,4 @@ def delete_book(book_id):
     db.session.delete(book)
     db.session.commit()
     
-    return make_response(f"Book {book.id} successfully deleted")
+    return make_response(jsonify(f"Book {book.id} successfully deleted"))
